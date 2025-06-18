30beb71a1f40dd2f9f56a90685f398bdaa3332fa
Roman Povoroznyk <roman.povorozny@patrianna.com>
2025-06-18 18:52:37 +0300

    feat: add structured logging with zerolog
    - Implement logger package with trace, debug, info, warn, error levels
    - Add console output with human-readable format
    - Integrate logger initialization in root command
    - Add logging demonstration in version command
    - Support structured logging with key-value fields

--- cmd/version.go
+++ cmd/version.go
@@ -1,9 +1,10 @@
 package cmd
 
 import (
-"fmt"
+	"fmt"
 
-"github.com/spf13/cobra"
+	"github.com/roman-povoroznyk/k6s/pkg/logger"
+	"github.com/spf13/cobra"
 )
 
 // versionCmd represents the version command
@@ -12,7 +13,13 @@ var versionCmd = &cobra.Command{
 	Short: "Print the version number of k6s",
 	Long:  `Print the version number and build information for k6s.`,
 	Run: func(cmd *cobra.Command, args []string) {
+		logger.Info("Displaying version information", map[string]interface{}{
+			"version": Version,
+			"command": "version",
+		})
 		fmt.Printf("k6s version %s\n", Version)
+		
+		logger.Debug("Version command completed successfully", nil)
 	},
 }
 
